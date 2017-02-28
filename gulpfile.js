587121d355708d6ea3feda03d8c484163b1a2a7c
Hannu Oksa <hannu.oksa@reaktor.fi>
2017-02-28 12:42:49 -0500

    Rename PostCSS directory for better readability

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('css', function() {
     precss
   ];
 
-  return gulp.src('./src/pcss/styles.css')
+  return gulp.src('./src/precss/styles.css')
     .pipe(postcss(processors))
     .pipe(gulp.dest('./src/css'));
 });
@@ -24,7 +24,7 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/pcss/**/*.css', ['css', browserSync.reload]);
+  gulp.watch('src/precss/**/*.css', ['css', browserSync.reload]);
   gulp.watch('src/js/**/*.js', browserSync.reload);
   gulp.watch('src/*.html', browserSync.reload);
 });
