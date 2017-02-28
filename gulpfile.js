92ea7cf715c6dbd7778e73d4d984a4a500963fa5
Hannu Oksa <hannu.oksa@reaktor.fi>
2017-02-28 10:28:59 -0500

    Static server and tasks for watching

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var precss = require('precss');
+var browserSync = require('browser-sync');
 
 gulp.task('css', function() {
   var processors = [
@@ -13,3 +14,19 @@ gulp.task('css', function() {
     .pipe(postcss(processors))
     .pipe(gulp.dest('./src/css'));
 });
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: './src/'
+    }
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/pcss/**/*.css', ['css', browserSync.reload]);
+  gulp.watch('src/js/**/*.js', browserSync.reload);
+  gulp.watch('src/*.html', browserSync.reload);
+});
+
+gulp.task('default', ['css', 'browser-sync', 'watch']);
